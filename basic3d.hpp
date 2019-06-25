3cb847cadc9a50a1dfea1f1e450a8f4d75ac9d9f
AlexTunder <rakutin2005@mail.ru>
2019-06-25 19:10:36 +0000

    just update

--- basic3d.hpp
+++ basic3d.hpp
@@ -1,4 +1,4 @@
-#include <GL/freeglut.h>
+#include <GL/glut.h>
 #include <fstream>
 using namespace std;
 //САНЯ БЛЯТЬ, ЗАПОМНИ:
@@ -8,7 +8,6 @@ using namespace std;
     координаты нормали (vn)
     Параметры одной поверхности объекта (f)
     Комментарии (#)
-
     v - описание соединения точек по id. пример:
     v id_точи_в_пространстве/id_точки_с_текстурой/id_с_нормалью
     т.е.:
@@ -45,50 +44,64 @@ namespace glClass
     };
     class obj{
         public:
+        int PolygobCount;
             enum {VERTEXESLASTID,NORMALESLASTID,ENDERDcLASTID};
         //Points decloration
-        Point *VERTEXES = new Point;
-        Point *NORMALES = new Point;
-        Point *TEX_VERT = new Point;
-        Polygon *RENDERDC = new Polygon; //x - VERTEX.point, y - NORMALES.point, z - TEX_VERT.point, id - id
+        Point VERTEXES[150];
+        Point NORMALES[150];
+        Point TEX_VERT[150];
+        Polygon RENDERDC[150]; //x - VERTEX.point, y - NORMALES.point, z - TEX_VERT.point, id - id
         //functions decloration
         int pushNewPoint(Point *VERTEX_TARGET, Point point);
-        int getObjFromFile(char *string);
+        int getObjFromFile(std::string string);
         char *FileWithTex;
     };
-    int obj::getObjFromFile(char *string){
+    int obj::getObjFromFile(std::string string){
         VERTEXES[0].id = NORMALES[0].id = TEX_VERT[0].id = RENDERDC[0].points[0].id = 0;
         ifstream fin(string);
         std::string substr;
-        while(fin.eof()){
+        if(!fin.is_open())
+            return 95;
+        while(!fin.eof()){
             fin >> substr;
             if(substr == "v"){
-                for(int i = 0; i<3; i++){
+                VERTEXES[0].id++;
                     fin >> substr;
-                    VERTEXES[++VERTEXES[0].id].x = std::stof(substr);
-                }
+                    VERTEXES[VERTEXES[0].id].x = std::stof(substr);
+                    fin >> substr;
+                    VERTEXES[VERTEXES[0].id].y = std::stof(substr);
+                    fin >> substr;
+                    VERTEXES[VERTEXES[0].id].z = std::stof(substr);
             }else if(substr == "vn"){
-                for(int i = 0; i<3; i++){
+                NORMALES[0].id++;
                     fin >> substr;
-                    NORMALES[++NORMALES[0].id].x = std::stof(substr);
-                }
+                    NORMALES[NORMALES[0].id].x = std::stof(substr);
+                    fin >> substr;
+                    NORMALES[NORMALES[0].id].y = std::stof(substr);
+                    fin >> substr;
+                    NORMALES[NORMALES[0].id].z = std::stof(substr);
             }else if(substr == "vt"){
-                for(int i = 0; i<3; i++){
+                TEX_VERT[0].id = 0;
                     fin >> substr;
-                    TEX_VERT[++TEX_VERT[0].id].x = std::stof(substr);
-                }
+                    TEX_VERT[TEX_VERT[0].id].x = std::stof(substr);
+                    fin >> substr;
+                    TEX_VERT[TEX_VERT[0].id].y = std::stof(substr);
+                    fin >> substr;
+                    TEX_VERT[TEX_VERT[0].id].z = std::stof(substr);
             }else if(substr == "f"){
+                PolygobCount = RENDERDC[0].points[0].id++;
                 for(int i = 0; i<4; i++){
                     fin >> substr;
-                    RENDERDC[RENDERDC[0].points[0].id].points[i].x = substr[0];
+                    RENDERDC[RENDERDC[0].points[0].id].points[i].x = substr[0]  - '0';
                     if(substr[2] != '/') {
-                        RENDERDC[RENDERDC[0].points[0].id].points[i].y = substr[2];
-                        RENDERDC[RENDERDC[0].points[0].id].points[i].z = substr[5];
-                    }else RENDERDC[RENDERDC[0].points[0].id].points[i].z = substr[3];
+                        RENDERDC[RENDERDC[0].points[0].id].points[i].y = substr[2] - '0';
+                        RENDERDC[RENDERDC[0].points[0].id].points[i].z = substr[5] - '0';
+                    }else RENDERDC[RENDERDC[0].points[0].id].points[i].z = substr[3] - '0';
                 }
             }
         }
     fin.close();
+    return 0;
     }
     int obj::pushNewPoint(Point *VERTEX_TARGET, Point point){
         VERTEX_TARGET[VERTEX_TARGET[0].id] = point;
@@ -97,15 +110,27 @@ namespace glClass
       public:
       obj main;
         void draw(){
-            glBegin(GL_POLYGON);
             //drawing start
-            for(int i = 1; i < main.RENDERDC[0].points[0].id; i++, glEnd())
-                for(int y = 0; y < 4; y++){
-                    /*TEXTURE:   ((main.TEX_VERT[main.RENDERDC[i].points[y].y].x,main.TEX_VERT[main.RENDERDC[i].points[y].y].y,main.TEX_VERT[main.RENDERDC[i].points[y].y].z))*/
-                    /*NORMALE:   ((main.NORMALES[main.RENDERDC[i].points[y].z].x,main.NORMALES[main.RENDERDC[i].points[y].z].y,main.NORMALES[main.RENDERDC[i].points[y].z].z))*/
-                        glVertex3f(main.VERTEXES[int(main.RENDERDC[i].points[y].x)].x,main.VERTEXES[int(main.RENDERDC[i].points[y].x)].y,main.VERTEXES[int(main.RENDERDC[i].points[y].x)].z);
-                    }
+            glColor3f(1,1,0);
+            for(int i = 1; i < main.PolygobCount; i++)
+            {
+                glBegin(GL_POLYGON);
+                
+                // for(int y = 0; y < 4; y++){
+                //     /*TEXTURE:   ((main.TEX_VERT[main.RENDERDC[i].points[y].y].x,main.TEX_VERT[main.RENDERDC[i].points[y].y].y,main.TEX_VERT[main.RENDERDC[i].points[y].y].z))*/
+                //     /*NORMALE:   ((main.NORMALES[main.RENDERDC[i].points[y].z].x,main.NORMALES[main.RENDERDC[i].points[y].z].y,main.NORMALES[main.RENDERDC[i].points[y].z].z))*/
+                //         glVertex3f(main.VERTEXES[int(main.RENDERDC[i].points[y].x)].x,main.VERTEXES[int(main.RENDERDC[i].points[y].x)].y,main.VERTEXES[int(main.RENDERDC[i].points[y].x)].z);
+                //     }
+                //     glEnd();
+
+                        glVertex3f(main.VERTEXES[int(main.RENDERDC[i].points[0].x)].x,main.VERTEXES[int(main.RENDERDC[i].points[0].x)].y,main.VERTEXES[int(main.RENDERDC[i].points[0].x)].z);
+                        glVertex3f(main.VERTEXES[int(main.RENDERDC[i].points[1].x)].x,main.VERTEXES[int(main.RENDERDC[i].points[1].x)].y,main.VERTEXES[int(main.RENDERDC[i].points[1].x)].z);
+                        glVertex3f(main.VERTEXES[int(main.RENDERDC[i].points[2].x)].x,main.VERTEXES[int(main.RENDERDC[i].points[2].x)].y,main.VERTEXES[int(main.RENDERDC[i].points[2].x)].z);
+                        glVertex3f(main.VERTEXES[int(main.RENDERDC[i].points[3].x)].x,main.VERTEXES[int(main.RENDERDC[i].points[3].x)].y,main.VERTEXES[int(main.RENDERDC[i].points[3].x)].z);
             //drawing end
+            glEnd();
+            }
+            
         }
     };
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
-#include <GL/freeglut.h>
+#include <GL/glut.h>
 #include <cstring.h>
 #include <stdio.h>
 #include <iostream>
-#include "scenes.hpp"
+#include "basic3d.hpp"
 glClass::model a;
+void display() {
+//     cout << a.main.VERTEXES[5].x;
+//     cout <<"\n"<< a.main.PolygobCount<<"\n";
+//     // a.main.getObjFromFile("Pathologic/resourses/untitled.obj");
+// //    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+//     glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+//     // glBegin(GL_POLYGON);
+//     glColor3f(1,1,0);
+//     a.draw();
+//     // glVertex3f(-1,-1,0);
+//     // glVertex3f(1,1,0);
+//     // glEnd();
+//     // glColor3f(1,1,0);
+//     // a.draw();
+//     glutSwapBuffers();
+}
 int main(int arg, char **argv){
-    a.main.getObjFromFile("resourses/untitled.obj");
-    cout << a.main.RENDERDC[2].points[2].x;
+  cout << a.main.getObjFromFile("Pathologic/resourses/untitled.obj");
+  // glutInit(&arg,argv);
+  // glutInitWindowSize( 500, 750 );       /* A x A pixel screen window  */
+
+  // glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE);
+  // glutCreateWindow("My Rectangle"); /* window title                   */
+  // glutDisplayFunc(display); 
+  // glClearColor(0, 0, 0, 0); /* gray background */
+ 
+  // glMatrixMode(GL_PROJECTION);      /* In World coordinates: */
+  // glLoadIdentity();                 /* position the "clipping rectangle" */
+  // glOrtho( -2,2, -2, 2,2,-2);/* at -B/2, its right edge at +B/2, its bottom */
+  // glMatrixMode(GL_MODELVIEW);       /* edge at -B/2 and its top edge at +B/2 */                      /* set attributes                 */
+
+  // glutMainLoop();                   /* pass control to the main loop  */
     return 0;
 }
